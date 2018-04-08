7ba2996c3fb3bdebb01def4596cd7cbbb4729d12
FallenTM <33818316+FallenTM@users.noreply.github.com>
2018-04-08 18:42:54 -0400

    Create clear.js

---
+++ commands/clear.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Sorry, but you cannot message manages.");
+  if(!args[0]) return message.channel.send("no");
+  message.channel.bulkDelete(args[0]).then(() => {
+  message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
+});
+module.exports.help = {
+  name: "clear"
+}
