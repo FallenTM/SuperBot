e409d385c5a7a74b8c46636ca2188f46b38d49ae
FallenTM <33818316+FallenTM@users.noreply.github.com>
2018-04-08 18:55:12 -0400

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
   if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Sorry, but you cannot message manages.");
   if(!args[0]) return message.channel.send("no");
   message.channel.bulkDelete(args[0]).then(() => {
-  message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
+  message.channel.send("Cleared: " + args[0] + " messages").then(msg => msg.delete(2000));
 });
 module.exports.help = {
   name: "clear"
