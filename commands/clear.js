73ac89be23ef4b905f756bcf93e677c61f225978
FallenTM <33818316+FallenTM@users.noreply.github.com>
2018-04-08 19:00:37 -0400

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Sorry, but you cannot manager messages.");
-  if(!args[0]) return message.reply("Sorry, but you must specify an amount of messages to delete.")
-  message.channel.bulkDelete(args[0]).then(() => {
-  message.channel.send("Cleared: " + args[0] + " messages").then(msg => msg.delete(2000));
-});
+  if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Sorry, but you cannot manage messages.");
+  if (!args[0]) return message.reply("Sorry, but you must specify an amount of messages to delete.");
+  message.channel.bulkDelete(args[0]);
+  await message.channel.send("Cleared: " + args[0] + " messages").then(msg => msg.delete(2000));
+};
 module.exports.help = {
   name: "clear"
 }
