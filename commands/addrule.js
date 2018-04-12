3eb87d8e9a89c50deac034c74eafc7512936d7e2
FallenTM <33818316+FallenTM@users.noreply.github.com>
2018-04-11 21:33:14 -0400

    Update addrule.js

--- commands/addrule.js
+++ commands/addrule.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   if (!message.member.hasPermission("ADMININSTRATOR")) return message.reply("Sorry, but you cannot do this.");
   let ruleChannel = message.guild.channels.find("name", "rules");
-  if (!ruleChannel) return message.reply("Sorry, but I could not find that channel.");
+  if (!ruleChannel) return message.reply("Sorry, but I could not find the #rules channel.");
 
   if (args.length == 0) {
     message.reply("Invalid Usage! !addrule <Rule>");
@@ -11,6 +11,8 @@ module.exports.run = async (bot, message, args) => {
   }
   let arg = args.join(" ").slice(22);
   ruleChannel.send(arg);
+  message.reply("Added rule!");
+  await message.delete(0);
 }
 
 module.exports.help = {
