f7e7269e0763bda4ee187681594195300cef24da
FallenTM <33818316+FallenTM@users.noreply.github.com>
2018-04-11 21:55:07 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,9 +5,10 @@ module.exports.run = async (bot, message, args) => {
     let embed = new Discord.RichEmbed()
     .setDescription("User Help")
     .setColor("#15f153")
-    .addField("Current commands", "!help and !report")
+    .addField("Current commands", "!help, !report, and !serverinfo")
     .addField("Help Command", "Gets all commands for this bot.")
-    .addField("Report Command", "Report a user and send it to an admin.");
+    .addField("Report Command", "Report a user and send it to an admin.")
+    .addField("Serverinfo Command", "Get all info on the server and yourself.");
 
     message.delete(0);
     message.reply("Help has been sent to your DM's");
@@ -16,14 +17,15 @@ module.exports.run = async (bot, message, args) => {
     let aembed = new Discord.RichEmbed()
     .setDescription("Admin Help")
     .setColor("#15f153")
-    .addField("Current commands", "!help, !report, !addrole, !clear, !kick, !removerole, !addrule")
+    .addField("Current commands", "!help, !report, !addrole, !clear, !kick, !removerole, !addrule, and !serverinfo")
     .addField("Help Command", "Gets all commands for this bot.")
     .addField("Report Command", "Report a user and send it to an admin.")
     .addField("Addrole Command", "Add a role to a user.")
     .addField("Clear command", "Clear a certain amount of messages in a channel.")
     .addField("Kick command", "Kick a person from the server")
     .addField("RemoveRole command", "Remove a role from a user.")
-    .addField("Addrule Command", "Add a rule to the #rules channel");
+    .addField("Addrule Command", "Add a rule to the #rules channel")
+    .addField("Serverinfo Command", "Get all info on the server and yourself.");
 
     message.delete(0);
     message.reply("Help has been sent to your DM's");
